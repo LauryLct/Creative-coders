0219bc493f774d9def56e1115ce149febcc97796
Laury Lecaut <laury.lecaut@gmail.com>
2021-04-09 18:16:28 +0200

    conatctus part

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="contact-us mt-5" id="4">
-    <h2>Nous contacter</h2>
-    <div class="">
+    <h2 class='p-3 m-3'>Nous contacter</h2>
+    <div class="phone p-4">
+      <img src="../assets/phone.svg" alt="phone-logo">
       <p>Telephone:</p>
+      <a class="p-3" href="tel:+33672265043">(+33)672265043</a>
     </div>
-    <a class="p-3" href="tel:+33672265043">(+33)672265043</a>
-    <p>E-mail:</p>
-    <a href="mailto:contact@creative-coders.fr">contact@creative-coders.fr</a>
+    <div class="email p-4">
+      <img src="../assets/1.svg" alt="email-logo">
+      <p>E-mail:</p>
+      <a href="mailto:contact@creative-coders.fr">contact@creative-coders.fr</a>
+    </div>
   </div>
 </template>
 
@@ -21,6 +25,7 @@ p {
   margin-bottom: O;
 }
 img {
-  width: 15px;
+  width: 40px;
+  fill: #e70000 !important;
 }
 </style>
