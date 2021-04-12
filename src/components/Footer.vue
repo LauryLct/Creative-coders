64eda97ba91c659f6b25f1041237773b6caf4fe0
LauryLct <laury.lecaut@gmail.com>
2021-04-12 17:23:35 +0200

    Add footer and add arrow link in header

---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="footer p-5">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h3>A propos</h3>
+                    <p>Creative Coders est une entreprise de création de site web sur-mesure. Nous créons des sites à votre image</p>
+                </div>
+                <div class="col">
+                    <h3>Contact</h3>
+                    <a class="p-3" href="tel:+33672265043">(+33)672265043</a>
+                    <a href="mailto:contact@creative-coders.fr">contact@creative-coders.fr</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+.footer {
+    background-color: #3A4750;
+}
+
+p {
+    color: white;
+}
+
+a {
+    color: white;
+}
+
+h3 {
+    color: white;
+    padding: 30px;
+}
+</style>
